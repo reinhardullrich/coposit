640bf70d0ca47d5b7633090fb65471e0e143f5c1
Reinhard Ullrich <reinhard.ullrich@fastmail.com>
2026-08-13 11:11:52 +0300

    Add integer matrix diagnostic formatting

--- cpp/tests/test_integer.cpp
+++ cpp/tests/test_integer.cpp
@@ -1,4 +1,5 @@
 #include <coposit/integer.hpp>
+#include <coposit/matrix_integer.hpp>
 
 #include <gtest/gtest.h>
 
@@ -16,4 +17,15 @@ TEST(IntegerTest, ConvertsArbitraryWidthTextInTheRequestedBase)
     EXPECT_EQ(value.to_string(2), bits);
     EXPECT_EQ(value.to_string(), "36893488147419103233");
     EXPECT_THROW(value.set_string("2", 2), std::invalid_argument);
+}
+
+TEST(MatrixIntegerTest, FormatsRowsForDiagnostics)
+{
+    matrix_integer matrix(2, 2);
+    matrix(0, 0) = integer(1);
+    matrix(0, 1) = integer(-2);
+    matrix(1, 0) = integer(-2);
+    matrix(1, 1) = integer(3);
+
+    EXPECT_EQ(matrix.to_pretty_string(), "  0: [1, -2]\n  1: [-2, 3]");
 }
